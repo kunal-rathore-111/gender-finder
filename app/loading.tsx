86f1e68037a6bfc12379699d7c53e7904a2320a9
Kunal-Rathore <kunalworkspace111@gmail.com>
2026-02-13 23:23:23 +0530

    Refactor HomePage component and add Loading component for improved user experience

---
+++ app/loading.tsx
@@ -0,0 +1,5 @@
+
+export default function Loading() {
+    return <div className="text-4xl font-black  min-h-screen flex items-center justify-center font-oswald animate-pulse">CALCULATING DESTINY...</div>
+
+}
